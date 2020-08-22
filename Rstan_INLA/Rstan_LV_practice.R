d028576d1d5fa5fd0104716ef6adc420906c0800
Liang_Mac <xl0418@gmail.com>
2020-08-22 10:32:41 +0200

    bug fixed

--- Rstan_INLA/Rstan_LV_practice.R
+++ Rstan_INLA/Rstan_LV_practice.R
@@ -1,13 +1,13 @@
 library(rstan)
 source('Lotka_volterra_sim.R')
-sim_time <- 100
+sim_time <- 21
 paras <- c(0.055, 0.0028, 0.084, .0026)
 z_ini <- c(50,50)
 z_sim <- Lotka_Volterra_sim(ts = sim_time, paras = paras, z_ini = z_ini)
 
 N <- nrow(z_sim) - 1
 time_series <- 1:N
-lv_data <- list(N = N, ts = time_series, y_ini = z_ini, y = z_sim[2:sim_time,])
+lv_data <- list(N = N, ts = time_series, y_ini = z_ini, y = z_sim[2:(N+1),])
 
-model <- rstan::stan_model("lotka_volterra_stan.stan")
-fit <- rstan::sampling(model = model, data = lv_data, seed = 18, iter=5000, chains=3, cores=3)
+model <- stan_model("Lotka_volterra_stan.stan")
+fit <- sampling(model, data = lv_data, seed = 18, iter=10000, chains=3, cores=3)
